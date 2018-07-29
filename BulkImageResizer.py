2c173cda017d1de35eccc0103df4db567b983cee
Suhrab Kurbanov <sir.suhrab@gmail.com>
2018-07-28 22:03:31 -0500

    set subsampling=0, added optimize=True both in imageResize() method. Completed imageCompress() method with quality set to 95.

--- BulkImageResizer.py
+++ BulkImageResizer.py
@@ -9,28 +9,18 @@ class Resizer():
         self.outputFolderPath = outputFolderPath
         self.imageFiles = [file for file in os.listdir(sourceFolderPath) if file.endswith(('.jpg', '.jpeg', '.png', '.gif', '.bmp', '.eps', '.j2k', '.j2p', '.jpx', '.icns', '.ico', '.pcx', '.webp', '.tiff', '.tif'))]
 
-    def imageResize(self, x_dimension, y_dimension, aspectRatio=False, quality=95):
+    def imageResize(self, x_dimension, y_dimension, aspectRatio=False, subsampling=0, quality=95):
         for file in self.imageFiles:
             img = Image.open(self.sourceFolderPath + file)
             if not aspectRatio:
                 img = img.resize((x_dimension, y_dimension), Image.ANTIALIAS)
-                img.save(self.outputFolderPath + file, format=img.format, quality=quality)
+                img.save(self.outputFolderPath + file, format=img.format, optimize=True, subsampling=subsampling, quality=quality)
             elif aspectRatio:
                 maxSize = max(x_dimension, x_dimension)
                 img.thumbnail((maxSize, maxSize), Image.ANTIALIAS)
-                img.save(self.outputFolderPath + file, format=img.format, quality=quality)
+                img.save(self.outputFolderPath + file, format=img.format, optimize=True, subsampling=subsampling, quality=quality)
 
-
-# def imageResizer()
-
-# for fn in os.listdir('images_src/'):
-#     if fn.endswith('.jpg'):
-#         img = Image.open('images_src/' + fn)
-#         if max(img.size) > 800:
-#             img.thumbnail((800, 800), Image.ANTIALIAS)
-#             img.save('images/' + fn, 'JPEG', quality=90)
-
-
-p1 = Resizer('/Users/SK/Desktop/GitHub/PythonProjects/BulkImageResizer/images_src/', '/Users/SK/Desktop/GitHub/PythonProjects/BulkImageResizer/dinara/')
-
-p1.imageResize(500, 600, quality=50)
+    def imageCompress(self, quality=95):
+        for file in self.imageFiles:
+            img = Image.open(self.sourceFolderPath + file)
+            img.save(self.outputFolderPath + file, format=img.format, optimize=True, quality=quality)
